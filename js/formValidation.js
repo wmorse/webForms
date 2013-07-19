945b19494fabeb86791a8057cc74845744217a65
Daniel <danrenaud08@gmail.com>
2013-07-19 11:29:22 -0400

    First Commit (test)
    No significant changes
    This is just to test the commit and pull request process
    
    - changed include path to work in Windows
    - added blank js file just for testing

---
+++ js/formValidation.js
@@ -0,0 +1,6 @@
+//Javascript file for form validation
+
+//Submitted forms will be sent to this script and checked for validity
+
+//Valid entries will be sent to PHP via AJAX request
+//Invalid entries will return the form to the user with error messages
